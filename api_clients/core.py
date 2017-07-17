4cca9121be37a3dcf6b865f3f4569b032083545b
dcgoss <dcgoss14@gmail.com>
2017-07-17 12:00:08 -0400

    Moved to CircleCI version 2

--- api_clients/core.py
+++ api_clients/core.py
@@ -7,18 +7,18 @@ class CoreClient(BaseAPIClient):
                             '/classifiers/queue/',
                             params={'title': titles, 'worker_id': self.worker_id, 'limit': limit})
 
-    def release_classifier(self, task):
+    def release_classifier(self, classifier):
         return self.request('post',
-                            '/classifiers/{id}/release/'.format(id=task['id']),
+                            '/classifiers/{id}/release/'.format(id=classifier['id']),
                             params={'worker_id': self.worker_id})
 
-    def fail_classifier(self, task):
+    def fail_classifier(self, classifier):
         return self.request('post',
-                            '/classifiers/{id}/fail/'.format(id=task['id']),
+                            '/classifiers/{id}/fail/'.format(id=classifier['id']),
                             params={'worker_id': self.worker_id})
 
-    def upload_notebook(self, task, notebook_output_path):
+    def upload_notebook(self, classifier, notebook_output_path):
         with open(notebook_output_path, mode='rb') as notebook_file:
             return self.request('post',
-                                '/classifiers/{id}/upload/'.format(id=task['id']),
+                                '/classifiers/{id}/upload/'.format(id=classifier['id']),
                                 files={'notebook_file': notebook_file})
