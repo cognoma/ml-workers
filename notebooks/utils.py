96043f75cd8939b3155646c8f1867430919b0f72
Patrick Miller <miller1417@gmail.com>
2017-10-04 17:55:52 -0400

    Update the notebook in production (#16)
    * Update the notebook in production
    * Use updated notebook from machine-learning repository
    * Remove Vega specs and import
    * Add in plotnine requirement

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,69 +1,22 @@
-def sanitize_dataframe(df):
-    """Sanitize a DataFrame to prepare it for serialization.
-    * Make a copy
-    * Raise ValueError if it has a hierarchical index.
-    * Convert categoricals to strings.
-    * Convert np.int dtypes to Python int objects
-    * Convert floats to objects and replace NaNs by None.
-    * Convert DateTime dtypes into appropriate string representations
+def theme_cognoma(fontsize_mult=1):   
+    import plotnine as gg
     
-    Args:
-        df (object): can be a variety of dataframe like objects
-        
-    Returns:
-        pandas.DataFrame: a dataframe that is ready to be serialized
-    """
-    import pandas as pd
-    import numpy as np
-
-    df = df.copy()
-
-    for col_name, dtype in df.dtypes.iteritems():
-        if str(dtype) == 'category':
-            # XXXX: work around bug in to_json for categorical types
-            # https://github.com/pydata/pandas/issues/10778
-            df[col_name] = df[col_name].astype(str)
-        elif np.issubdtype(dtype, np.integer):
-            # convert integers to objects; np.int is not JSON serializable
-            df[col_name] = df[col_name].astype(object)
-        elif np.issubdtype(dtype, np.floating):
-            # For floats, convert nan->None: np.float is not JSON serializable
-            col = df[col_name].astype(object)
-            df[col_name] = col.where(col.notnull(), None)
-        elif str(dtype).startswith('datetime'):
-            # Convert datetimes to strings
-            # astype(str) will choose the appropriate resolution
-            df[col_name] = df[col_name].astype(str).replace('NaT', '')
-    return df
-
-def fill_spec_with_data(spec, data):
-    """Take a Vega specification with missing data elements and complete it
-    * Uses an incomplete Vega specification that has named data arguments
-        which have empty values and replaces those missing values with
-        data elements passed in
-    * The Vega specification needs to have named data arguments that match
-        the data that is passed in to this function
-        
-    Args:
-        spec (dictionary): the Vega specification with incomplete data
-        data (named dictionary of pandas.DataFrame): data elements
-        
-    Returns:
-        dictionary: a completed Vega specification ready to be used
-    """
-    data_names = [d['name'] for d in spec['data']]
-    
-    for name, df in data.items():
-        if name not in data_names:
-            raise ValueError('Name not in data spec')
-        for ix in range(len(data_names)):
-            if data_names[ix] == name:
-                break
-                
-        df = sanitize_dataframe(df)
-        spec['data'][ix]['values'] = df.to_dict(orient='records')
-
-    return spec
+    return (gg.theme_bw(base_size = 14 * fontsize_mult) +
+        gg.theme(
+          line = gg.element_line(color = "#4d4d4d"), 
+          rect = gg.element_rect(fill = "white", color = None), 
+          text = gg.element_text(color = "black"), 
+          axis_ticks = gg.element_line(color = "#4d4d4d"),
+          legend_key = gg.element_rect(color = None), 
+          panel_border = gg.element_rect(color = "#4d4d4d"),  
+          panel_grid = gg.element_line(color = "#b3b3b3"), 
+          panel_grid_major_x = gg.element_blank(),
+          panel_grid_minor = gg.element_blank(),
+          strip_background = gg.element_rect(fill = "#FEF2E2", color = "#4d4d4d"),
+          axis_text = gg.element_text(size = 12 * fontsize_mult, color="#4d4d4d"),
+          axis_title_x = gg.element_text(size = 13 * fontsize_mult, color="#4d4d4d"),
+          axis_title_y = gg.element_text(size = 13 * fontsize_mult, color="#4d4d4d")
+    ))
 
 def get_model_coefficients(classifier, feature_set, covariate_names):
     """
@@ -103,3 +56,53 @@ def get_model_coefficients(classifier, feature_set, covariate_names):
     
     
     return coef_df
+
+def get_genes_coefficients(pca_object, classifier_object,
+                           expression_df, expression_genes_df,
+                           num_covariates=None):
+    """Identify gene coefficients from classifier after pca.
+
+    Args:
+        pca_object: The pca object from running pca on the expression_df.
+        classifier_object: The logistic regression classifier object.
+        expression_df: The original (pre-pca) expression data frame.
+        expression_genes_df: The "expression_genes" dataframe used for gene
+                             names.
+        num_covariates: Optional, only needed if PCA was only performed on a
+                        subset of the features. This should be the number of
+                        features that PCA was not performed on. This function
+                        assumes that the covariates features were at the end.
+
+    Returns:
+        gene_coefficients_df: A dataframe with entreze gene-ID, gene name,
+                            coefficient abbsolute value of coefficient, and 
+                            gene description. The dataframe is sorted by 
+                            absolute value of coefficient.
+    """
+
+    import pandas as pd
+
+    # Get the classifier coefficients.
+    if num_covariates:
+        coefficients = classifier_object.coef_[0][0:-num_covariates]
+    else:
+        coefficients = classifier_object.coef_[0]
+    # Get the pca weights.
+    weights = pca_object.components_
+    # Combine the coefficients and weights.
+    gene_coefficients = weights.T @ coefficients.T
+    # Create the dataframe with correct index
+    gene_coefficients_df = pd.DataFrame(gene_coefficients, columns=['weight'])
+    gene_coefficients_df.index = expression_df.columns
+    gene_coefficients_df.index.name = 'entrez_id'
+    expression_genes_df.index = expression_genes_df.index.map(str)
+    # Add gene symbol and description
+    gene_coefficients_df['symbol'] = expression_genes_df['symbol']
+    gene_coefficients_df['description'] = expression_genes_df['description'] 
+    # Add absolute value and sort by highest absolute value.
+    gene_coefficients_df['abs'] = gene_coefficients_df['weight'].abs()
+    gene_coefficients_df.sort_values(by='abs', ascending=False, inplace=True)
+    # Reorder columns
+    gene_coefficients_df = gene_coefficients_df[['symbol', 'weight', 'abs',
+                                                'description']]
+    return(gene_coefficients_df)
