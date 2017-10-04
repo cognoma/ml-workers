67f517a4fc0c28ba0633e607ee2acc72e0dd644e
Patrick Miller <miller1417@gmail.com>
2017-10-04 17:44:21 -0400

    Add code to test the classifier notebook (#18)
    * Add code to test the classifier notebook and update README

--- test_ml_task_runner.py
+++ test_ml_task_runner.py
@@ -1,3 +1,20 @@
+import os
 from ml_task_runner import run_notebook
 
+print('Testing download notebook.')
 run_notebook('1.download')
+
+os.environ['gene_ids'] = '7157-7158-7159-7161'
+os.environ['disease_acronyms'] = 'ACC-BLCA'
+
+print('Testing classifier notebook.')
+try:
+    notebook_output_path = run_notebook('2.mutation-classifier', timeout=3*60*60)
+    print('Machine learning completed.')
+except MemoryError as error:
+    print(error)
+except Exception as error:
+    print('Failed to complete classifier.')
+    print(error)
+
+print('Check notebook output in folder output.')
